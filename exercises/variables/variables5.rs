881e97fd2f3407a0d7abef928e939428c76ded45
wdf <wdf>
2023-08-06 11:36:51 +0800

    1

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -7,7 +7,10 @@
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
-    println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    println!("Spell a Number : {}", number); 
+    {
+        let number = 3; // don't rename this variable
+        println!("Number plus two is : {}", number + 2);
+    }
+    
 }
